e904b589922e0a9f2ad2a51a92b7e1212f757006
Prashant Sahu <psahu559@gmail.com>
2022-06-10 11:59:40 +0530

    Kotlin basics

--- app/src/main/java/com/publicissapient/PSAndroidKotlinApp/MainActivity.kt
+++ app/src/main/java/com/publicissapient/PSAndroidKotlinApp/MainActivity.kt
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val fruits = listOf<String>("mango", "banana", "pineapple", "orange","watermelon")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CallAFunction();
+        println(callAFunction(5,5).toString())
+        printFruitsArray()
     }
 
-    private fun CallAFunction() {
-        TODO("Not yet implemented")
+    private fun printFruitsArray() {
+        for (item in fruits){
+            println("Current Fruit is: $item")
+        }
+        //To get index
+        for(index in fruits.indices){
+            println("Current Fruit is : ${fruits[index]}")
+        }
+    }
+
+    private fun callAFunction(a: Int ,b: Int ):Int {
+        return  a+b
     }
 }
