73f3e199e7243eca4dc568f310fb33f558baeb66
Prashant Sahu <psahu559@gmail.com>
2022-06-13 11:44:12 +0530

    Splash Screen Implemented

---
+++ app/src/main/java/com/publicissapient/PSAndroidKotlinApp/Activity/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.publicissapient.PSAndroidKotlinApp.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.publicissapient.PSAndroidKotlinApp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startActivity( Intent (this, MainActivity::class.java))
+        finish()
+    }
+}
